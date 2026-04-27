405293e21cfa693663502640e8470a7a262036b9
sohan24-dev <sohan.explorer@gmail.com>
2026-04-28 02:55:00 +0600

    add

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,10 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://try-test-google-login.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
+
+
+// http://localhost:3000
